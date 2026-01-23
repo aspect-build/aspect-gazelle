e7e8f0228da846e49df249f79b1b26ac2222a378
Jason Bedard <jason@aspect.build>
2026-01-23 14:02:06 -0800

    feat: support assets from jsx elements (#176)
    ### Changes are visible to end-users: yes
    
    - Searched for relevant documentation and updated as needed: yes
    - Breaking change (forces users to change their own code or config): no
    - Suggested release notes appear below: yes
    
    Support `ts_project(assets)` from JSX elements such as `<img src>`,
    `<source src>` etc.
    
    ### Test plan
    
    - Covered by existing test cases
    - New test cases added
    
    ---------
    
    Co-authored-by: jbedard <89246+jbedard@users.noreply.github.com>

--- language/js/tests/tsx_css_outdir/main.tsx
+++ language/js/tests/tsx_css_outdir/main.tsx
@@ -2,3 +2,9 @@ import "./styles.css?no-inline"
 import "./styles.css#no-inline"
 import "./styles.css"
 import "./sub/lib"
+
+export const App = () => (
+  <div>
+    <img src="./images/logo.webp" />
+  </div>
+)
