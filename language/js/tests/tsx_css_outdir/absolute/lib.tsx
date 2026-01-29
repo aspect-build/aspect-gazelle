d8d1f0a25e2d164b2f591937c3908b9cffba9881
Jason Bedard <jason@aspect.build>
2026-01-28 17:44:34 -0800

    fix: absoltue jsx assets are relative to package.json

---
+++ language/js/tests/tsx_css_outdir/absolute/lib.tsx
@@ -0,0 +1,12 @@
+// Test JSX img src with absolute paths (resolved relative to workspace root)
+
+export const AbsoluteImages = () => (
+  <div>
+    {/* Absolute path to root-level image */}
+    <img src="/images/logo.webp" />
+    {/* Absolute path to root-level image with query param */}
+    <img src="/logo.png?v=1" />
+    {/* Absolute path to sibling directory's image */}
+    <img src="/sub/images/icon.gif" />
+  </div>
+)
