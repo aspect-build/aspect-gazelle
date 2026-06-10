0b62a03e72807d247228ad63453d6d839d4d09cd
Jason Bedard <jason@aspect.build>
2026-06-10 12:44:51 -0700

    refactor: remove unnecessary parallelization of jq/yq/regex/toml queries (#377)
    The overlap of error groups and per-file parallelization causes
    confusion and potentially perf/io issues.
    
    ### Changes are visible to end-users: no
    
    ### Test plan
    
    - Covered by existing test cases

--- language/orion/queries/toml.go
+++ language/orion/queries/toml.go
@@ -2,11 +2,9 @@ package queries
 
 import (
 	"bytes"
-	"sync"
 
 	"github.com/aspect-build/aspect-gazelle/language/orion/plugin"
 	"github.com/mikefarah/yq/v4/pkg/yqlib"
-	"golang.org/x/sync/errgroup"
 )
 
 func runTomlQueries(fileName string, sourceCode []byte, queries plugin.NamedQueries) (plugin.QueryResults, error) {
@@ -21,30 +19,12 @@ func runTomlQueries(fileName string, sourceCode []byte, queries plugin.NamedQuer
 	}
 
 	results := make(plugin.QueryResults, len(queries))
-	var mu sync.Mutex
-
-	eg := errgroup.Group{}
-	eg.SetLimit(10)
-
 	for key, q := range queries {
-		// Capture loop variables for goroutine
-		key := key
-		q := q
-		eg.Go(func() error {
-			r, err := runYamlQuery(node, q.(*plugin.TomlQuery).Query)
-			if err != nil {
-				return err
-			}
-
-			mu.Lock()
-			results[key] = r
-			mu.Unlock()
-			return nil
-		})
-	}
-
-	if err := eg.Wait(); err != nil {
-		return nil, err
+		r, err := runYamlQuery(node, q.(*plugin.TomlQuery).Query)
+		if err != nil {
+			return nil, err
+		}
+		results[key] = r
 	}
 	return results, nil
 }
