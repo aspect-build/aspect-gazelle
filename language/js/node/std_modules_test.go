6d3345e4569cf044e2fa79dd9c4c2e6286e8747e
Jason Bedard <jason@aspect.build>
2026-06-11 17:32:25 -0700

    perf: use map[string]struct{} for the Node builtin-module set (#385)
    ### Changes are visible to end-users: no
    
    ### Test plan
    
    - Covered by existing test cases
    - New test cases added

---
+++ language/js/node/std_modules_test.go
@@ -0,0 +1,40 @@
+package gazelle
+
+import (
+	"testing"
+)
+
+func TestIsNodeImport(t *testing.T) {
+	builtins := []string{
+		"fs",
+		"fs/promises",
+		"path",
+		"assert",
+		"assert/strict",
+		"child_process",
+		"util",
+		// "node:" prefixed imports are always builtins
+		"node:fs",
+		"node:path",
+		"node:test",
+	}
+	for _, m := range builtins {
+		if !IsNodeImport(m) {
+			t.Errorf("IsNodeImport(%q) = false, want true", m)
+		}
+	}
+
+	nonBuiltins := []string{
+		"",
+		"react",
+		"@scope/pkg",
+		"./fs",
+		"fs2",
+		"lodash/fp",
+	}
+	for _, m := range nonBuiltins {
+		if IsNodeImport(m) {
+			t.Errorf("IsNodeImport(%q) = true, want false", m)
+		}
+	}
+}
