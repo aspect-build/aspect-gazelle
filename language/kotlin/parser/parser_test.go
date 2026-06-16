a4d2db4b6e9b65a67c88d37710b33c8c7967c2fe
Jason Bedard <jason@aspect.build>
2026-06-15 17:07:09 -0700

    restore some test code to reduce diff

--- language/kotlin/parser/parser_test.go
+++ language/kotlin/parser/parser_test.go
@@ -150,7 +150,10 @@ typealias AliasedInt = Int
 func TestTreesitterParser(t *testing.T) {
 	for _, tc := range testCases {
 		t.Run(tc.desc, func(t *testing.T) {
-			res, _ := NewParser().Parse(tc.want.File, []byte(tc.kt))
+			res, errs := NewParser().Parse(tc.want.File, []byte(tc.kt))
+			if len(errs) > 0 {
+				t.Errorf("Errors parsing %q: %v", tc.want.File, errs)
+			}
 
 			tc.want.sort()
 			got := makeComparable(res)
@@ -222,24 +225,33 @@ func TestNewSimpleIdentifier(t *testing.T) {
 
 func TestMainDetection(t *testing.T) {
 	t.Run("main detection", func(t *testing.T) {
-		res, _ := NewParser().Parse("main.kt", []byte("fun main() {}"))
+		res, errs := NewParser().Parse("main.kt", []byte("fun main() {}"))
+		if len(errs) > 0 {
+			t.Errorf("Parse error: %v", errs)
+		}
 		if !res.HasMain {
 			t.Errorf("main method should be detected")
 		}
 
-		res, _ = NewParser().Parse("x.kt", []byte(`
+		res, errs = NewParser().Parse("x.kt", []byte(`
 package my.demo
 fun main() {}
 		`))
+		if len(errs) > 0 {
+			t.Errorf("Parse error: %v", errs)
+		}
 		if !res.HasMain {
 			t.Errorf("main method should be detected with package")
 		}
 
-		res, _ = NewParser().Parse("x.kt", []byte(`
+		res, errs = NewParser().Parse("x.kt", []byte(`
 package my.demo
 import kotlin.text.*
 fun main() {}
 		`))
+		if len(errs) > 0 {
+			t.Errorf("Parse error: %v", errs)
+		}
 		if !res.HasMain {
 			t.Errorf("main method should be detected with imports")
 		}
